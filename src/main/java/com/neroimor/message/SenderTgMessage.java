f1d02a02880b77cc780d61e177e4ec77d28a4450
Neroimor <neroimor@gmail.com>
2025-01-28 16:07:42 +0300

    added class for generate QR code

--- src/main/java/com/neroimor/message/SenderTgMessage.java
+++ src/main/java/com/neroimor/message/SenderTgMessage.java
@@ -1,8 +1,12 @@
 package com.neroimor.message;
 
+import com.google.zxing.WriterException;
+import com.neroimor.Generator.GenerateQrCode;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -11,7 +15,8 @@ public class TextMessage {
     HashMap <String, String> text_map;
     public TextMessage() {
         text_map = new HashMap();
-        text_map.put("/start", "Привет, я твой бот для генерации qr кодов!!!");
+        text_map.put("/start", "Привет, я твой бот для генерации qr кодов!!!\n" +
+                "Просто пришли мне текст и я преобразую его в qr код");
         text_map.put("hello", "Привет");
     }
 
@@ -22,11 +27,12 @@ public class TextMessage {
         sendMessage.setText(text_map.get("/start"));
         return sendMessage;
     }
-    public SendMessage sendMassageDefault(Update update){
+    public SendPhoto sendMassageQrCode(Update update) throws WriterException, IOException {
         String chatId = update.getMessage().getChatId().toString();
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(chatId);
-        sendMessage.setText(text_map.get("hello"));
-        return sendMessage;
+        String data = update.getMessage().getText();
+        SendPhoto send = new SendPhoto();
+        send.setChatId(chatId);
+        send.setPhoto(new GenerateQrCode().generateQR(data));
+        return send;
     }
 }
