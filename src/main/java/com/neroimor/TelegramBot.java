4d7e2252b59ac469988d077dcf160b28bba8d474
Neroimor <neroimor@gmail.com>
2025-01-28 15:40:00 +0300

    added response to start

--- src/main/java/com/neroimor/TelegramBot.java
+++ src/main/java/com/neroimor/TelegramBot.java
@@ -1,11 +1,14 @@
 package com.neroimor;
 
+import com.neroimor.message.TextMessage;
 import com.neroimor.settings.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
 public class TelegramBot extends TelegramLongPollingBot {
@@ -23,6 +26,15 @@ public class TelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         if(update.hasMessage() && update.getMessage().hasText()){
             String message = update.getMessage().getText();
+            SendMessage sendMessage = switch (message){
+                case "/start" -> new TextMessage().sendMassage(update);
+                default -> new TextMessage().sendMassageDefault(update);
+            };
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
             log.info(message);
         }
     }
--- src/main/java/com/neroimor/message/TextMessage.java
+++ src/main/java/com/neroimor/message/TextMessage.java
@@ -1,2 +1,32 @@
-package com.neroimor.message;public class TextMessage {
+package com.neroimor.message;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.HashMap;
+
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TextMessage {
+    HashMap <String, String> text_map;
+    public TextMessage() {
+        text_map = new HashMap();
+        text_map.put("/start", "Привет, я твой бот для генерации qr кодов!!!");
+        text_map.put("hello", "Привет");
+    }
+
+    public SendMessage sendMassage(Update update){
+        String chatId = update.getMessage().getChatId().toString();
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(text_map.get("/start"));
+        return sendMessage;
+    }
+    public SendMessage sendMassageDefault(Update update){
+        String chatId = update.getMessage().getChatId().toString();
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(text_map.get("hello"));
+        return sendMessage;
+    }
 }
